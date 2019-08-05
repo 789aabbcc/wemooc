cb1422e3afc7f9ee399f1ad0a9524fe37c9b94ae
李彦俊 <1175411086@qq.com>
2019-08-05 19:54:15 +0800

    引入elementUI

---
+++ src/screen/class/class-detail1.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="class-detail-container">
+     <div class="class-detail-session1"></div>
+     <div class="class-detail-session2"></div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+    @import '../../common/sass/base.scss';
+    .class-detail-container{
+        width:100%;
+        .class-detail-session1{
+            width:100%;
+            height:290*$length;
+            @include bgImage('');
+            margin: 0 auto;
+            .class-detail-session1-center{
+                width:1200*$length;
+                margin:0 auto;
+            }
+        }
+        .class-detail-session2{
+            width:1200*$length;
+            margin:0 auto;
+        }
+    }
+</style>
